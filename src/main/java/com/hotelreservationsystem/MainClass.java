032f2b739b8b15749310bf23d3bd022e8f6a4b03
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-15 09:32:58 +0530

    ADD: welcome note for hotel reservation problem

---
+++ src/main/java/com/hotelreservationsystem/MainClass.java
@@ -0,0 +1,7 @@
+package com.hotelreservationsystem;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System!");
+    }
+}
