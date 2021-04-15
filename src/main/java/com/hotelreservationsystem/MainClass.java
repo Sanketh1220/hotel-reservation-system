a26a09fcfc12e56808e836c4830a067e53a6f6a6
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-15 09:47:38 +0530

    ADD: hotel names and their prices

--- src/main/java/com/hotelreservationsystem/MainClass.java
+++ src/main/java/com/hotelreservationsystem/MainClass.java
@@ -3,5 +3,11 @@ package com.hotelreservationsystem;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System!");
+
+        HotelReservationSystem hotelSystem = new HotelReservationSystem();
+        hotelSystem.addHotel(new HotelClass("Lakewood", 110));
+        hotelSystem.addHotel(new HotelClass("Lakewood", 160));
+        hotelSystem.addHotel(new HotelClass("Lakewood", 220));
+        System.out.println("Hotels are added!");
     }
 }
