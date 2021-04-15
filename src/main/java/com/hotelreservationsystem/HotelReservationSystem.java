abb13c825f4077db880e39760acfddc17ba9f4f3
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-15 19:02:00 +0530

    ADD: added rates, ability to get rate for given range

--- src/main/java/com/hotelreservationsystem/HotelReservationSystem.java
+++ src/main/java/com/hotelreservationsystem/HotelReservationSystem.java
@@ -1,8 +1,13 @@
 package com.hotelreservationsystem;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
 
 public class HotelReservationSystem {
+    private SimpleDateFormat date = new SimpleDateFormat("ddMMMyyyy");
     private ArrayList<HotelClass> hotelClassList = new ArrayList<>();
 
     public HotelReservationSystem() {
@@ -11,5 +16,19 @@ public class HotelReservationSystem {
 
     public void addHotel(HotelClass hotelClass) {
         hotelClassList.add(hotelClass);
+    }
+
+    public HotelClass getCheapestHotel(String checkinDate, String checkoutDate) {
+        try {
+            Date checkin = date.parse(checkinDate);
+            Date checkout = date.parse(checkoutDate);
+            long days = (long) ((checkout.getTime() - checkin.getTime()) / (86.4e6)) +1;
+            System.out.println("Days = " + days);
+            return hotelClassList.stream().sorted(Comparator.comparingLong(hotelClass ->
+                    (hotelClass).calculateTotalPrice(days))).findFirst().orElse(null);
+        } catch (ParseException e) {
+            System.out.println("Exception Occured is " + e);
+        }
+        return null;
     }
 }
