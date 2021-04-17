b421cd9c366f81665d73aeeba533c83b4966c5af
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-17 08:32:21 +0530

    ADDed: test case

--- src/test/java/com/hotelreservationsystem/HotelReservationSystemTest.java
+++ src/test/java/com/hotelreservationsystem/HotelReservationSystemTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HotelReservationSystemTest {
     @Test
-    public void testGetCheapestHotelForDateRange() {
+    public void GivenInput_ForGetCheapestHotel_ForDateRange() {
         HotelReservationSystem obj  = new HotelReservationSystem();
         assertEquals("Lakewood, Total Rates: $220", obj.getCheapestHotel("10Sep2020", "11Sep2020").toString());
     }
